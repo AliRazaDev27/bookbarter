27f4bfaea00202036f7160e22dd99c5bb8cc3ff5
AliRaza <alirazadev27@gmail.com>
2025-04-25 11:06:10 +0500

    FIX: user login zod schema

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -20,11 +20,14 @@ import { setUserData } from "@/store/features/user/userSlice"
 import { useToast } from "@/hooks/use-toast"
 
 const schema = z.object({
-  email: z.string().email("Invalid email format"),
+  email: z.string()
+  .min(6, "Email must be at least 6 characters long")
+  .max(254, "Email must not exceed 254 characters")
+  .email("Invalid email format"),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
-    .max(32, "Password must not exceed 32 characters")
+    .max(64, "Password must not exceed 64 characters")
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/\d/, "Password must contain at least one number")
