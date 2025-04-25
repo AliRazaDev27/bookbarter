94c4b0eb289598ff8e89ceb7a956c036b1fada98
AliRaza <alirazadev27@gmail.com>
2025-04-25 11:05:08 +0500

    FIX: user login zod schema

--- backend/src/config/zodSchemas/userLoginZodSchema.ts
+++ backend/src/config/zodSchemas/userLoginZodSchema.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 const schema = z.object({
-  email: z.string().nonempty("Email cannot be empty").email("Invalid email format"),
+  email: z.string()
+  .nonempty("Email cannot be empty")
+  .min(6, "Email must be at least 6 characters long")
+  .max(254, "Email must not exceed 254 characters")
+  .email("Invalid email format"),
   password: z
     .string()
     .nonempty("Password cannot be empty")
     .min(8, "Password must be at least 8 characters long")
-    .max(32, "Password must not exceed 32 characters")
+    .max(64, "Password must not exceed 32 characters")
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/\d/, "Password must contain at least one number")
